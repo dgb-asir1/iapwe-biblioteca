762aa30a55a3b6b32f83ee84dd96519a416f09c8
dgb-asir1 <damasogb58@educastur.es>
2026-01-31 22:38:04 +0100

    consultar nombre acorde a nuevo nombre tablas

--- web_biblioteca/comprobar_login.php
+++ web_biblioteca/comprobar_login.php
@@ -6,7 +6,7 @@
     $nombre_usuario = $_POST["nombre_usuario"];
     $password = $_POST["password"];
 
-    $consulta = "SELECT * FROM usuarios WHERE nombre_usuario = ?";
+    $consulta = "SELECT * FROM Usuarios WHERE nombre_usuario = ?";
     $sentencia = $conexion->prepare($consulta);
     $sentencia->bind_param("s", $nombre_usuario);
     $sentencia->execute();
