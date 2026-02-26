dc9e8cff8b6be5c66ce7429d30e08b8ced4b3869
dgb-asir1 <damasogb58@educastur.es>
2026-02-26 20:28:02 +0100

    quitado registro de usuarios (no de clientes) en index

--- web_biblioteca/index.php
+++ web_biblioteca/index.php
@@ -10,18 +10,14 @@
 <body>
     <h2>BIBLIOTECA IAPWE - ACCESO</h2>
 
-<br>
-<h3>Introduzca sus credenciales</h3>
-<form action="comprobar-login.php" method="POST">
-    <input type="text" name="nombre_usuario" placeholder="Usuario" required>
-    <br><br>
-    <input type="password" name="password" placeholder="Contraseña" required>
-    <br><br>
-    <input type="submit" value="Iniciar sesión">
-</form>
-<!-- mensaje error -->
-<br>
-<a href="registro.html">Registrarse</a>
-</body>
+    <br>
+    <h3>Introduzca sus credenciales</h3>
+    <form action="comprobar-login.php" method="POST">
+        <input type="text" name="nombre_usuario" placeholder="Usuario" required>
+        <br><br>
+        <input type="password" name="password" placeholder="Contraseña" required>
+        <br><br>
+        <input type="submit" value="Iniciar sesión">
+    </form>
 
 </html>
